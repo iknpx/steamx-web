d01680757a72335bffbe6e1e6a593f14a9459879
Iakov Salikov <isalikov@exadel.com>
2018-06-27 15:02:57 +0300

    feat: Assets

--- src/app/routes/StartPage/index.js
+++ src/app/routes/StartPage/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+
 import style from './style.styl';
+import maxresdefault from '@assets/images/maxresdefault.jpg';
 
 export default class StartPage extends Component {
     render() {
         return (
             <div className={style.container}>
-                ...
+                <img src={maxresdefault} alt="maxresdefault" />
             </div>
         );
     }
--- webpack/index.js
+++ webpack/index.js
@@ -12,6 +12,7 @@ const options = {
         modules: ['node_modules', 'src'],
         alias: {
             '@': path.resolve(__dirname, '../src/app'),
+            '@assets': path.resolve(__dirname, '../src/assets'),
             '@style': path.resolve(__dirname, '../src/stylus'),
         },
     },
